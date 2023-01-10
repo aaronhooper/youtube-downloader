9e66e886ef4646ef8d9662a1b3d3247621d5c5df
Aaron Hooper <rnjckhpr@gmail.com>
2023-01-10 23:37:12 +0000

    Add download route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
+const fs = require('fs');
+const ytdl = require('ytdl-core');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'YouTube Downloader' });
+});
+
+router.get('/download', function(req, res, next) {
+  const url = req.query.url;
+  const videoName = 'video.mp4';
+  const writeStream = fs.createWriteStream(videoName);
+  ytdl(url).pipe(writeStream);
+
+  res.send('it worked!');
 });
 
 module.exports = router;
